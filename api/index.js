1dec16692c28e241ae2ba1db48fa358952182c27
Thomas Maximiliano Mintzer <111892223+ThoMintz@users.noreply.github.com>
2024-04-03 17:15:56 -0300

    pongo el type json

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import datos from "./datos.json";
+import datos from "./datos.json" assert { type: 'json' };;
 const cors = require('cors');
 const express = require('express');
 import path, { dirname } from "path";
