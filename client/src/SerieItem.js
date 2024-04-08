808e54c6395bf2e1f994d104b1ef22251e232a88
Thomas Maximiliano Mintzer <111892223+ThoMintz@users.noreply.github.com>
2024-04-08 17:27:03 -0300

    Prueba de componente serieItem

---
+++ client/src/SerieItem.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './List.css';
+
+function SerieItem({ serie, index }) {
+    return (
+        <li className="li-items">
+            <div className="accordion-item">
+                <h2 className="accordion-header">
+                    <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target={`#collapse${index}`}
+                        aria-expanded="false"
+                        aria-controls={`collapse${index}`}
+                    >
+                        {serie.nombre}
+                    </button>
+                </h2>
+                <div className="accordion-collapse collapse" id={`collapse${index}`} data-bs-parent="#lista-series">
+                    <div className="accordion-body">
+                        <span className="nombre">{serie.nombre}</span>
+                        <br />
+                        <div className="content_image">
+                            <span>{serie.sinopsis}</span>
+                            <br />
+                            <span className="Imagen-Cards">
+                                <img src={serie.imagen} alt={serie.nombre} />
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </li>
+    );
+}
+
+export default SerieItem;
