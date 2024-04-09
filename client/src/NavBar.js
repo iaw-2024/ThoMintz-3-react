093f1387aedcab333c9891657a5074c2dd4d1872
Thomas Maximiliano Mintzer <111892223+ThoMintz@users.noreply.github.com>
2024-04-09 17:21:56 -0300

    Quito el boton del navbar

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -6,10 +6,6 @@ function NavBar() {
             <a className="navbar-brand" href="https://www.cartoonnetwork.com.ar/">
                 <img src="images/cartoonLogo.png" className="Imagen-Cards" alt="Un Show Mas" style={{ width: '150px', height: 'auto' }} />
             </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item active">
